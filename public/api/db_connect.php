7068a23a2f925a6771a4b7d65a25abcdc294e69e
Sean Prouty <prouty_411@hotmail.com>
2018-08-15 18:11:33 -0700

    received data from server

---
+++ public/api/db_connect.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once('../../config/db.php');
+
+$conn = mysqli_connect($host,$user,$password,$db_name);
+
+if(!$conn){
+    $output['error'] = 'Error conncecting to the database';
+    print(json_encode($output));
+    exit;
+}
